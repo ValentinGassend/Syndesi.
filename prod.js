43eec97840a5ae4b794bab82dbf712031c406b8b
ValentinGassend <valentin.gassend@gmail.com>
2022-12-07 01:58:02 +0100

    little fix

--- prod.js
+++ prod.js
@@ -8,6 +8,6 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
