e0617a8eca64d00b5b20150229dc0e447872a4c0
ValentinGassend <valentin.gassend@gmail.com>
2022-12-07 03:25:32 +0100

    Fix all trouble for vercel

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const path = require("path");
+
+import express from "express";
+import path from "path";
 const app = express();
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
-app.use("/static/image", express.static(path.resolve(__dirname, "frontend", "static/image")));
-app.use('/scss', express.static(path.join(__dirname, '../static/css')))
+
+app.use("/static", express.static(path.resolve("./", "frontend", "static")));
+app.use("/static/image", express.static(path.resolve("./", "frontend", "static/image")));
+app.use('/scss', express.static(path.join("./", '../static/css')))
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve("./frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
-module.exports = app;
